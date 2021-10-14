d6474fff921fbf4ebff0d85cb6c195319ddf5ff6
nukopy <pytwbf201830@gmail.com>
2021-10-15 06:27:44 +0900

    test: add test for empty stack

--- datastructures/stack/stack_test.go
+++ datastructures/stack/stack_test.go
@@ -12,21 +12,28 @@ func TestStack(t *testing.T) {
 	s.Push(3)
 	s.Push(5)
 	if s.Len() != 3 {
-		t.Errorf("Stack length should be 3, got %d", s.length)
+		t.Errorf("Length of stack should be 3, got %d", s.length)
 	}
 
 	// test Top
 	if s.Top() != 5 {
-		t.Errorf("Stack top should be 5, got %d", s.Top())
+		t.Errorf("Top value of stack should be 5, got %d", s.Top())
 	}
 
 	// test Pop
 	s.Pop()
-	v := s.Pop()
-	if v == nil || v != 3 {
-		t.Errorf("Stack pop should be 3, got %d", v)
+	v1 := s.Pop()
+	if v1 == nil || v1 != 3 {
+		t.Errorf("v1 should be 3, got %d", v1)
 	}
 	if s.Len() != 1 {
-		t.Errorf("Stack length should be 1, got %d", s.Len())
+		t.Errorf("Length of stack should be 1, got %d", s.Len())
+	}
+
+	// test Pop of empty stack
+	s.Pop()
+	v2 := s.Pop()
+	if v2 != nil && s.Len() != 0 {
+		t.Errorf("Value of pop of empty stack should be nil, got %d", v2)
 	}
 }
